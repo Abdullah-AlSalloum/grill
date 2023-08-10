a2e4adcd29baac08043e0d2497d015685dafa407
Abdullah <abdullahalsalloum06@gmail.com>
2023-08-10 23:27:15 +0300

    second commint

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,111 @@ const toggleNavbar = function () {
 }
 
 addEventOnElements(navTogglers , "click" , toggleNavbar)
+
+
+
+/*
+   *Header
+ */
+
+
+  const header = document.querySelector("[data-header]")
+  
+  let lastScrollPos = 0
+
+
+  const hideHeader = function (){
+    const isScrollBottom = lastScrollPos <= window.scrollY
+
+    if(isScrollBottom){
+      header.classList.add('hide')
+    }else{
+      header.classList.remove('hide')
+    }
+
+    lastScrollPos = window.scrollY;
+  }
+
+   
+
+  window.addEventListener('scroll' , function (){
+    if(window.scrollY >= 50){
+      header.classList.add("active")
+      hideHeader()
+    }else{
+      header.classList.remove("active")
+    }
+  })
+
+
+
+  /*
+     *HERO SLIDER 
+  */
+
+
+
+  const heroSlider = document.querySelector("[data-hero-slider]")
+  const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]")
+  const heroSliderPrevBtn = document.querySelector("[data-prev-btn]")
+  const heroSliderNextBtn = document.querySelector("[data-next-btn]")
+
+
+  let currentSliderPos = 0
+  let lastActiveSliderItem = heroSliderItems[0]
+
+  const updateSliderPos = function (){
+    lastActiveSliderItem.classList.remove("actvie")
+    heroSliderItems[currentSliderPos].classList.add("active")
+    lastActiveSliderItem = heroSliderItems[currentSliderPos]
+  }
+
+  const slideNext = function (){
+    if(currentSliderPos >= heroSliderItems.length - 1){
+      currentSliderPos = 0
+    }else{
+      currentSliderPos++;
+    }
+
+    updateSliderPos()
+  }
+
+  heroSliderNextBtn.addEventListener("click" , slideNext)
+
+
+  const slidePrev = function (){
+    if(currentSliderPos <= 0){
+      currentSliderPos = heroSliderItems.length - 1
+    }else{
+      currentSliderPos--
+    }
+    updateSliderPos()
+  }
+
+  heroSliderPrevBtn.addEventListener("click" , slidePrev)
+
+
+
+  /*
+      *Auto Slide 
+   */
+
+
+
+  let autoSliderInterval;
+
+  const autoSlide = function (){
+    autoSliderInterval = setInterval(function(){
+      slideNext()
+    },7000)
+  }
+
+  addEventOnElements([heroSliderNextBtn , heroSliderPrevBtn] , "mouseover" , function () {
+    clearInterval(autoSliderInterval)
+  })
+
+
+  addEventOnElements([heroSliderNextBtn , heroSliderPrevBtn] , "mouseout" ,autoSlide)
+
+
+  window.addEventListener("load" , autoSlide)
